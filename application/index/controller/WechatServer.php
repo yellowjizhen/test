b4ca907ca5763d93ac316a148b2f228635aa1dfc
huangjizhen <270124823@qq.com>
2018-08-25 17:20:33 +0800

    1

--- application/index/controller/WechatServer.php
+++ application/index/controller/WechatServer.php
@@ -30,7 +30,37 @@ class WechatServer
         $app = Factory::officialAccount($config);
 
         $app->server->push(function ($message) {
-            return "您好！我是黄老吉";
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                case 'file':
+                    return '收到文件消息';
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+
+            // ...
         });
 
         $response = $app->server->serve();
